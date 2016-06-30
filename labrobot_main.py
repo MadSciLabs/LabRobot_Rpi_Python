e662cb2c6eff2615a404443e41dcf511772ab2d4
root <Adam Lassy>
2016-06-30 20:45:20 +0000

    update

--- labrobot_main.py
+++ labrobot_main.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import curses
 
 from array import *
 
@@ -12,44 +13,95 @@ sys.path.append(spacebrew_path)
 import roboclaw
 from pySpacebrew.spacebrew import Spacebrew
 
-def press(event):
+stdscr = curses.initscr()
+curses.cbreak()
+stdscr.keypad(1)
+stdscr.refresh()
 
-    print('press', event.key)
-    sys.stdout.flush()
+_valMotor = array('i',[0,0])
+_targetMotor = array('i',[0,0])
 
-    if event.key == 'i':
+_moveType = 0
+_running = True
 
-    	_keySpeed += .05
-    	if _keySpeed > 1
-    		_keySpeed = 1
+def initDir(_tMoveType):
+  
+  global _moveType, _targetMotor
 
-    if event.key == 'k':
+  if _tMoveType != _moveType:
 
-    	_keySpeed -= .05
-    	if _keySpeed < -1
-    		_keySpeed = -1
+    _moveType = _tMoveType
 
-    if event.key == 'j':
+    _targetMotor[0] = 0
+    _targetMotor[1] = 0
 
-    	_keyTurn -= .05
-    	if _keyTurn < -1
-    		_keyTurn = -1
+def addSpeed(_i, _s):
 
-    if event.key == 'l':
+  global _targetMotor, MOTOR_MAX
 
-    	_keyTurn += .05
-    	if _keyTurn > 1
-    		_keyTurn = 1
+  _t = _targetMotor[_i] + _s
 
-    if _keyTurn < 0:
+  if _t > MOTOR_MAX:
+    _t = MOTOR_MAX
 
-		_valMotor[0] = _keySpeed * MOTOR_MAX
-		_valMotor[1] = (1-abs(_keyTurn)) * _keySpeed * MOTOR_MAX
+  if _t < -MOTOR_MAX:
+    _t = -MOTOR_MAX
 
-	else:
+  _targetMotor[_i] = int(_t)
 
-		_valMotor[0] = (1-_keyTurn) * _keySpeed * MOTOR_MAX
-		_valMotor[1] = _keySpeed * MOTOR_MAX
+
+def main(stdscr):
+
+  global _targetMotor
+  global _moveType
+
+  _speedAlpha = 5
+
+  _k = ''
+  _motor0 = 0;
+  _motor1 = 0;
+
+  _keySpeed = 0.0
+  _keyTurn = 0.0
+
+  stdscr.nodelay(1)
+
+  while True:
+	
+    _k = stdscr.getch()
+    if _k != -1:
+      print(str(_k))
+
+      if _k == ord('i'):
+
+	initDir(0)
+
+        addSpeed(0,_speedAlpha)
+        addSpeed(1,_speedAlpha)
+
+      if _k == ord('k'):
+
+	initDir(0)
+
+        addSpeed(0,-_speedAlpha)
+        addSpeed(1,-_speedAlpha)
+
+      if _k == ord('l'):
+
+	initDir(1)
+
+        addSpeed(0,_speedAlpha)
+        addSpeed(1,-_speedAlpha)
+
+      if _k == ord('j'):
+
+	initDir(1)
+
+        addSpeed(0,-_speedAlpha)
+        addSpeed(1,_speedAlpha)
+
+    #print str(_targetMotor[0]) + ' ' + str(_targetMotor[1])
+    setMotors()
 
 
 # get app name and server from query string
@@ -65,8 +117,8 @@ brew.addSubscriber("motorB", "range")
 MOTOR_MIN = 0
 MOTOR_MAX = 70
 
-_valMotor = array('i',[0,0])
-_targetMotor = array('i',[0,0])
+ENABLE_MOTORS = True
+
 
 #for i in range(2):
 #	_valMotor[i] = 0
@@ -105,64 +157,63 @@ brew.subscribe("motorB", motor2)
 brew.subscribe("motorA", motor1)
 
 #Linux comport name
-roboclaw.Open("/dev/ttyACM0",115200)
+if ENABLE_MOTORS == True:
+    roboclaw.Open("/dev/ttyACM0",115200)
 
 address = 0x80
 _val = 0
-_alpha = 2
+_alpha = 5
 _speed = _alpha
 _stage = 0
 
-_keySpeed = 0
-_keyTurn = 0
+IS_KEYBOARD = True
+IS_SOCKETS = False
 
-IS_KEYBOARD = true
-
-if IS_KEYBOARD == true:
-
-	#FOR BOTH MOTORS
-	for i in range(2):
-		if _valMotor[i] > 0:
-			roboclaw.ForwardM1(address,_valMotor[i])
-		else:
-			roboclaw.BackwardM1(address,abs(_valMotor[i]))
-
-else:
+if IS_SOCKETS == True:
 
 	try:
-
 		brew.start()
-
-		while(1):
-
-			#FOR BOTH MOTORS
-			for i in range(2):
-				_diff = _targetMotor[i] - _valMotor[i]
-
-				if abs(_diff) > _alpha:
-			
-					if _diff > 0:
-						_valMotor[i] = _valMotor[i] + _alpha	
-					else:
-						_valMotor[i] = _valMotor[i] - _alpha	
-
-				print "tar " + str(i) + ": " + str(_targetMotor[i]) + " val : " + str(_valMotor[i])
-
-				if i == 0:
-					if _valMotor[i] > 0:
-		 				roboclaw.ForwardM1(address,_valMotor[i])
-					else:
-						roboclaw.BackwardM1(address,abs(_valMotor[i]))
-			else:
-				if _valMotor[i] > 0:
-		 			roboclaw.ForwardM2(address,_valMotor[i])
-				else:
-					roboclaw.BackwardM2(address,abs(_valMotor[i]))
-
-			time.sleep(.03)
-
 	finally:
 		brew.stop()
 
+#Set data
+#def setSpeed():
 
+def setMotors():
+
+	global _targetMotor, _valMotor, ENABLE_MOTORS
+
+	#FOR BOTH MOTORS
+	for i in range(2):
+
+		_diff = _targetMotor[i] - _valMotor[i]
+
+		if abs(_diff) >= _alpha:
+
+			if _diff > 0:
+				_valMotor[i] = _valMotor[i] + _alpha	
+			else:
+				_valMotor[i] = _valMotor[i] - _alpha	
+
+
+			if ENABLE_MOTORS == True: 
+				if i == 0:
+					if _valMotor[i] > 0:
+	 					roboclaw.ForwardM1(address,_valMotor[i])
+					else:
+						roboclaw.BackwardM1(address,abs(_valMotor[i]))
+				else:
+					if _valMotor[i] > 0:
+						roboclaw.BackwardM2(address,_valMotor[i])
+					else:
+		 				roboclaw.ForwardM2(address,abs(_valMotor[i]))	
+
+			time.sleep(.03)
+
+	print ">" + str(_valMotor[0]) + " " + str(_valMotor[1])
+
+if __name__ == '__main__':
+	curses.wrapper(main)
+
+curses.endwin()
 
